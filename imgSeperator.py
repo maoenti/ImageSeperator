5b20e315a08a07d007c541e78aea9adfbe43ea83
Melvyn <melvyndede@gmail.com>
2022-04-13 02:42:50 +0700

    removing unused function and comments. making a new make_newdir function

--- imgSeperator.py
+++ imgSeperator.py
@@ -4,28 +4,6 @@ from matplotlib import pyplot
 import os
 import shutil
 
-# # draw an image with detected objects
-# def draw_image_with_boxes(filename, result_list):
-#     # load the image
-#     data = pyplot.imread(filename)
-
-#     # plot the image
-#     pyplot.imshow(data)
-
-#     # get the context for drawing boxes
-#     ax = pyplot.gca()
-
-#     # plot each box
-#     for result in result_list:
-#         # get coordinates
-#         x, y, width, height = result['box']
-#         # create the shape
-#         rect = Rectangle((x, y), width, height, fill = False, color = 'red')
-#         #draw the box
-#         ax.add_patch(rect)
-#     #show the plot
-#     pyplot.show()
-
 def face_detection(file):
     # load image from file
     pixels = pyplot.imread(file)
@@ -33,10 +11,15 @@ def face_detection(file):
     faces = detector.detect_faces(pixels)
     return len(faces)
 
-dir = 'twicetagram'
-onedir = '\\twicetagram\\onemember'
-moredir = '\\twicetagram\\moremember'
+def make_newdir(dir):
+    os.mkdir(dir + '\\noface')
+    os.mkdir(dir + '\\more')
+
+dir = '..\\twicetagram'
+nofaceDir = dir + '\\noface'
+moreDir = dir + '\\more'
 cwd = os.getcwd()
+
 # create the detector, using default weights
 detector = MTCNN()
 counter = 1
@@ -52,6 +35,3 @@ for filename in os.listdir(dir):
         print(f'{counter}. Image {filename} has been moved successfully')
         # print(f'Image {f} has been moved successfully to {cwd + newdir}')
         counter += 1
-
-# # display faces on the original image
-# draw_image_with_boxes(filename, faces)

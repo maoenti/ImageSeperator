5a8891c58ad21dc797b0f1c766fbedcf72a885e0
Melvyn <melvyndede@gmail.com>
2022-10-23 02:48:30 +0700

    add progress bar

--- imgSeperator.py
+++ imgSeperator.py
@@ -21,28 +21,31 @@ def check_file_ext(file):
         return True
 
 # Hyperparameters
-dir = '..\\hi_sseulgi'
+dir = '..\\tzuyutwicee_'
 nofaceDir = dir + '\\noface'
 moreDir = dir + '\\more'
 cwd = os.getcwd()
-make_newdir(dir)
+# make_newdir(dir)
 
 # create the detector, using default weights
 detector = MTCNN()
 counter = 1
+check = 0
 for filename in os.listdir(dir):
     if check_file_ext(filename) == True:
         f = os.path.join(dir, filename)
-        if os.path.isfile(f) and face_detection(f) < 1:     # If the image doesn't contain any face
+        if os.path.isfile(f) and face_detection(f) == 1:     # If the image doesn't contain any face
             shutil.move(f, nofaceDir)
-            print(f'{len(os.listdir(dir)) - counter}. Image {filename} has been moved successfully')
-            # print(f'Image {f} has been moved successfully to {cwd + newdir}')
+            check += 1
+            # print(f'{len(os.listdir(dir)) - counter}. Image {filename} has been moved successfully')
         elif os.path.isfile(f) and face_detection(f) > 1:      # If the image contain more than one faces
             shutil.move(f, moreDir)
-            print(f'{len(os.listdir(dir)) - counter}. Image {filename} has been moved successfully')
-            # print(f'Image {f} has been moved successfully to {cwd + newdir}')
+            # print(f'{len(os.listdir(dir)) - counter}. Image {filename} has been moved successfully')
         else:
-            print(f'{len(os.listdir(dir)) - counter} files left')
+            # print(f'{len(os.listdir(dir)) - counter} files left')
             counter += 1
     else:
         pass
+    print(f'{1200 - check} files left')
+    if check > 1200:
+        break
